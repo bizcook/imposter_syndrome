194bf4dc71fc7a91d1b29c2b27d53bfa50a555ad
Biz Cook <elizabeth.myles.cook@gmail.com>
2016-04-14 13:03:03 -0500

    added a responsive header

--- server/public/assets/scripts/controllers.js
+++ server/public/assets/scripts/controllers.js
@@ -1,6 +1,43 @@
+// nav bar code
+
+$(window).resize(function() {
+  var more = document.getElementById("js-centered-more");
+  var windowWidth = $(window).width();
+  var moreLeftSideToPageLeftSide = $(more).offset().left;
+  var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+  if (moreLeftSideToPageRightSide < 330) {
+    $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
+    $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
+  }
+
+  if (moreLeftSideToPageRightSide > 330) {
+    $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
+    $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
+  }
+});
+
+$(document).ready(function() {
+  $(window).trigger("resize");
+  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
+  $("#js-centered-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-centered-navigation-menu").slideToggle(function(){
+      if($("#js-centered-navigation-menu").is(":hidden")) {
+        $("#js-centered-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+});
+
+
+//// controllers START
 myApp.controller("AddController", ["$scope", "$http", "SmartService", function($scope, $http, SmartService){
   $scope.smarts = {};
   $scope.data = [];
+
 
 
 
